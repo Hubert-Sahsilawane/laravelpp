8d74f48a6257525e015b768e37c2468fab3b403b
Hubert-Sahsilawane <susisusilowati4334@gmail.com>
2025-07-11 14:46:28 +0700

    menambahkan data person

---
+++ app/Http/Controllers/Api/PersonController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Person;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $person = Person::all();
+        return response()->json([
+            'status' => true,
+            'message' => 'data berhasil ditemukan',
+            'data' => $person
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation failed',
+                'error' => $validator->errors()
+            ], 422);
+        }
+
+        $person = Person::create($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => 'Person created successfully',
+            'data' => $person
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $person = Person::findOrFail($id);
+        return response()->json([
+            'status' => true,
+            'message' => 'data berhasil ditemukan',
+            'data' => $person
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation failed',
+                'error' => $validator->errors()
+            ], 422);
+        }
+
+        $person = Person::findOrFail($id);
+        $person -> update($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => 'Person created successfully',
+            'data' => $person
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $person = Person::findOrFail($id);
+        $person->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Berhasil Dihapus',
+        ], 204);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Api\PersonController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::resource('/person', PersonController::class);
