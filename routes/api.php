d5a32c56e584afdc864c355c66d7cbacfcbedad5
SahaHungkul <mhasbysyabani27@gmail.com>
2025-07-15 14:57:26 +0700

    menambahkan controller pada pelanggan

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\PersonController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PelangganController ;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +15,10 @@ Route::resource('/person', PersonController::class);
 Route::post('/user/register', [UserController::class, 'register']);
 Route::post('/user/login', [UserController::class, 'login']);
 Route::middleware('auth:api')->post('/user/logout', [UserController::class, 'logout']);
+
+// PELANGGAN (CRUD via API)
+Route::get('/pelanggan', [PelangganController::class, 'index']);
+Route::post('/pelanggan', [PelangganController::class, 'store']);
+Route::get('/pelanggan/{id}', [PelangganController::class, 'show']);
+Route::put('/pelanggan/{id}', [PelangganController::class, 'update']);
+Route::delete('/pelanggan/{id}', [PelangganController::class, 'destroy']);
